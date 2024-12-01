a550511b4e207387cc6dddeaeadf6c27fdd2da8a
Shiho <shiho.jo@yahoo.com>
2024-12-01 16:37:19 +0000

    Initialised gradle for root and backend modules

---
+++ backend/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    application
+    java
+}
+
+group = "org.uturano"
+version = "1.0-Ihumke-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass.set("org.uturano.Main")
+}
+
+dependencies {
+    // Using JavaParser and JavaSymbolSolver to do code comparison
+    implementation("com.github.javaparser:javaparser-symbol-solver-core:3.26.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+}
---
+++ backend/src/main/java/org/uturano/Main.java
@@ -0,0 +1,7 @@
+package org.uturano;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("There's nothing here now. Come back later.");
+	}
+}
